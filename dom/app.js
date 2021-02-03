3aa7d7cfe2f93084af3f63792bd120e7e79f0399
jthanawat <kla13thanawat@gmail.com>
2021-02-03 20:11:49 +0700

    getElementsByTagName&ClassName

--- dom/app.js
+++ dom/app.js
@@ -1,3 +1,22 @@
 document.getElementById('bear-photo');
 
 document.getElementById('main');
+
+document.getElementsByTagName('li');
+
+document.getElementsByTagName('h1');
+
+const inputs = document.getElementsByTagName('input');
+//get all inputs
+inputs[0];
+inputs.pop(); //DOES NOT WORK!
+
+document.getElementsByClassName('special');
+
+const specials = document.getElementsByClassName('special');
+for (let el of specials) {
+  console.log(el);
+}
+
+const arr = [...specials];
+arr.pop(); //this works!
