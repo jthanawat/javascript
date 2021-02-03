f50f14dd9627ea970edb44969d978735e659ba30
jthanawat <kla13thanawat@gmail.com>
2021-02-03 11:31:47 +0700

    add function shuffle

--- challenge/deck.js
+++ challenge/deck.js
@@ -12,3 +12,86 @@ function makeDeck() {
   }
   return deck;
 }
+
+function drawCard(deck) {
+  return deck.pop();
+}
+
+function drawMultiple(numCards, deck, drawnCards) {
+  const cards = [];
+  for (let i = 0; i < numCards; i++) {
+    cards.push(drawCard(deck, drawnCards));
+  }
+  return cards;
+}
+
+function shuffle(deck) {
+  // loop over array backwards
+  for (let i = deck.length - 1; i > 0; i--) {
+    //pick random index before current element
+    let j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
+}
+
+// We have to create a bunch of variables:
+const firstDeck = initializeDeck();
+const drawnCards = [];
+shuffle(firstDeck);
+// We have to pass a ton of arguments around:
+const hand1 = drawMultiple(2, firstDeck, drawnCards);
+const hand2 = drawMultiple(2, firstDeck, drawnCards);
+const pokerHand = drawMultiple(5, firstDeck, drawnCards);
+
+// **********************************
+// USING AN OBJECT + METHODS INSTEAD:
+// **********************************
+const myDeck = {
+  deck: [],
+  drawnCards: [],
+  suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+  values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+  initializeDeck() {
+    const { suits, values, deck } = this;
+    for (let value of values.split(',')) {
+      for (let suit of suits) {
+        this.deck.push({
+          value,
+          suit,
+        });
+      }
+    }
+    // return deck;
+  },
+
+  drawCard() {
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+    return card;
+  },
+  drawMultiple(numCards) {
+    const cards = [];
+    for (let i = 0; i < numCards; i++) {
+      cards.push(this.drawCard());
+    }
+    return cards;
+  },
+  shuffle() {
+    const { deck } = this;
+    // loop over array backwards
+    for (let i = deck.length - 1; i > 0; i--) {
+      //pick random index before current element
+      let j = Math.floor(Math.random() * (i + 1));
+      //swap
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+  },
+};
+
+myDeck.initializeDeck();
+myDeck.shuffle();
+const h1 = myDeck.drawMultiple(2);
+const h2 = myDeck.drawMultiple(2);
+const h3 = myDeck.drawMultiple(5);
